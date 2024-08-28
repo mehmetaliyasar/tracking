64e670db00a57f631b6b68fb155beccbc9336546
34rbn <mimarmehmetali35@gmail.com>
2024-08-28 18:42:21 +0300

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const statusOpt=["Mülakat","Devam Ediyor","Reddedildi"]
+export const typeOpt=["Tam Zamanlı", "Yarı Zamanlı","Uzaktan","Staj"]
+export const sortOpt = ['A-Z', 'Z-A', 'En Yeni', 'En Eski'];
